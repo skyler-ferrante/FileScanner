23d2f65d80d1857eab8a4d64886d9422f33fa9a5
Skyler Ferrante <sjf5462@rit.edu>
2021-08-22 22:09:41 -0400

    Created info.py

---
+++ info.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+from database.database import Database
+
+if __name__ == "__main__":
+    # filescanner = FileScanner()
+    database = Database()
+
+    files = database.get_all_files().fetchall()
+
+    directories = database.get_directories().fetchall()
+    for directory in directories:
+        print(directory[0])
+    
+    print()
+    print(len(files), "files")
+    print(len(directories), "directories")
