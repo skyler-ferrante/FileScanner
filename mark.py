e3e10b7649f21f9640c09bbc8bdc4fcbe19739c2
Skyler Ferrante <sjf5462@rit.edu>
2021-08-24 22:28:28 -0400

    Made usage much nicer

--- mark.py
+++ mark.py
@@ -1,28 +1,39 @@
 #!/bin/python3
 from main.filescanner import FileScanner
 import sys
+import os
 
 if __name__ == "__main__":
     filescanner = FileScanner()    
 
     if len(sys.argv) == 1:
         print("Usage:")
-        print(sys.argv[0], "file            Mark file")
-        print(sys.argv[0], "directory -r    Mark directory recursive")
+        print(sys.argv[0], "file            Mark files or directories")
         print(sys.argv[0], "-u              Update")
-        sys.exit(1)
-    filenames = sys.argv[1:]
+        sys.exit(0)
+    args = sys.argv[1:]
 
-    if "-u" in filenames:
+    if "-u" in args:
+        args.remove("-u")
         print("Updating...")
         filescanner.update_file_hashes()
 
-    elif "-r" in filenames:
-        filenames.remove("-r")
-        for file in filenames:
-            filescanner.mark_directory_recursive(file)
-    else:
-        print(len(filenames), "files")
-        filescanner.mark_files(filenames)
+    files = []
+    directories = []
+    unknowns = []
+
+    for arg in args:
+        if os.path.isfile(arg):
+            files.append(arg)
+        elif os.path.isdir(arg):
+            directories.append(arg)
+        else:
+            unknowns.append(arg)
+
+    filescanner.mark_files(files)
+    filescanner.mark_directories_recursive(directories)
+
+    for unknown in unknowns:
+        print(unknown, "is neither a known flag, file, or directory")
 
     filescanner.show_database_hash()
