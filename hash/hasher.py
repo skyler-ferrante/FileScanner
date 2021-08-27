904a465004f59b604874fc29cb4fce4104bdb564
Skyler Ferrante <sjf5462@rit.edu>
2021-08-27 14:15:41 -0400

    Uses less ram, and seems faster for small files
    Nedds more testing to determine which size is best

--- hash/hasher.py
+++ hash/hasher.py
@@ -11,16 +11,12 @@ def hash_file(filename, hasher = hashlib.sha256) -> str:
     
     #Open file in binary mode
     with open(filename, "rb") as file:
-        #Read whole file, faster in testing, but uses more ram
-        data = file.read()
-        hasher.update(data)
-
-        #Read file in 65536 byte chunks
-        # while True:
-        #     data = file.read(65536)
-        #     if not data:
-        #         break
-        #     hasher.update(data)
+        # Read file in 65536 byte chunks
+        while True:
+            data = file.read(65536)
+            if not data:
+                break
+            hasher.update(data)
     
     return hasher.hexdigest()
 
