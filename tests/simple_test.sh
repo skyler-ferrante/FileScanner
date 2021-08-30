6a2d897dab28ca9e8e00689ac5dea0b28b9876df
skyler-ferrante <sjf5462@rit.edu>
2021-08-29 20:26:40 -0400

    At some point I will write real tests
    This is really not great, but it works well enough for now.

---
+++ tests/simple_test.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+#Not great, but better then nothing
+
+#Move main.db (if exists) to main.db.bak
+if test -f "main.db";
+then
+    mv main.db main.db.bak
+fi
+
+#Mark /tmp/ and get amount of files changed
+./mark.py /tmp/
+amount_changed=$(./check.py -c | wc -l)
+
+#Since nothing happended between marking and checking, should be 0
+if test $amount_changed -ne 0; 
+then
+    echo "Nothing changed failed, $amount_changed change[s]"
+fi
+
+#Make a directory in /tmp/, and put a file test with contents hi in it
+TMPDIR=$(mktemp -d)
+echo "Hi" > $TMPDIR/test
+
+#Since we created a file, there should be one change
+amount_changed=$(./check.py -c | wc -l)
+if test $amount_changed -ne 1;
+then
+    echo "Changed one file failed, $amount_changed change[s]"
+fi
+
+#Try to find file with hash c01a4cfa25cb895cdd0bb25181ba9c1622e93895a6de6f533a7299f70d6b0cfb
+#(file with contents 'Hi')
+rm main.db
+./mark.py /tmp/
+find_hash=$(./check.py -h c01a4cfa25cb895cdd0bb25181ba9c1622e93895a6de6f533a7299f70d6b0cfb | awk '{print $2}')
+if test "$find_hash" = "$TMPDIR/test":
+then
+    echo "Looked for files with content Hi and got $find_hash"
+fi
+
+#Clean up
+rm $TMPDIR -r
+
+#Check how many directories registered, should be one (/tmp/)
+directories=$(./info.py | tail -n 1 | awk '{print $1}')
+if test $directories -ne 1;
+then
+    echo "Should have one directory, $directories found"
+fi
+
+#Remove main.db created during this script
+rm main.db
+#If main.db.bak exists, move back to main.db
+if test -f "main.db.bak";
+then
+    mv main.db.bak main.db
+fi
